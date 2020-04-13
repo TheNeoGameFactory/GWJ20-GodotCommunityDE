669a8e92fe54c8ddcc8696fa8e6c452d1fe542d2
Christoph Winkler <support@neo-gamefactory.de>
2020-04-13 12:08:36 +0200

    Init Grundprojekt

---
+++ Lab Rat - GWJ 2020/Scenes/Beschreibung.gd
@@ -0,0 +1,6 @@
+extends Node
+# Das ist einfach nur eine Beschreibung
+#
+# Hier kommen die Scenen rein, am besten gleich mit den Skripten, weil ich fand immer
+# das es sonst schwierig war bei Godot die skripte wieder zu finden,
+# vielleicht hat jemand noch eine bessere Idee.
