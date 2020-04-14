90782044eddd5c81a7173ead4042e2a95de9c02a
jonathan-wolt <jon.woltereck@web.de>
2020-04-14 12:39:42 +0200

    johns ratte und labyrinth eingebaut; startszene: mainmenu; Anfang eines Skriptes zum Mainmenu

---
+++ Lab Rat - GWJ 2020/Scenes/Menus/HauptMenu/HauptMenu.gd
@@ -0,0 +1,19 @@
+extends MarginContainer
+
+
+var labyrinth = "res://Scenes/testszene_für_ratten-movement.tscn"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_NewGame_button_down():
+	get_tree().change_scene(labyrinth)
