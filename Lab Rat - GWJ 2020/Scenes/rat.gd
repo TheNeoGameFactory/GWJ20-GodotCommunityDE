b1cc17f706f4eb4f2cc9de9e53c54b1affe177cd
jonathan-wolt <jon.woltereck@web.de>
2020-04-14 15:56:36 +0200

    Canomi-Finde-Skript im Autoload (name: canomi_autoload.gd)

--- Lab Rat - GWJ 2020/Scenes/rat.gd
+++ Lab Rat - GWJ 2020/Scenes/rat.gd
@@ -1,21 +1,17 @@
 extends KinematicBody
 
-
-
 var speed = 0.1
 
-
 func _physics_process(delta):
-	var move = Vector3(0,0,0)
+	var move = Vector3(0,-1,0)
 	if Input.is_action_pressed("ui_up"):
-		move += Vector3(1,0,0)
+		move.x += 1
 	elif Input.is_action_pressed("ui_down"):
-		move += Vector3(-1,0,0)
+		move.x += -1
 	if Input.is_action_pressed("ui_right"):
 		self.rotation.y -= 1 * delta
 	elif Input.is_action_pressed("ui_left"):
 		self.rotation.y += 1 * delta
-	
 
 	move = move.rotated(Vector3(0,1,0).normalized(),rotation.y)
 	move = move_and_collide(move*speed)
