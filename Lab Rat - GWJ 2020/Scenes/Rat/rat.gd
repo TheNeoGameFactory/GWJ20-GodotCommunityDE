1dab419857564cc10617d9bb5d34cd0291527ca7
HugeGameArtGD <hugegameartgd@github.com>
2020-04-15 17:19:26 +0200

    Kamera und Rotation vereinfacht

--- Lab Rat - GWJ 2020/Scenes/Rat/rat.gd
+++ Lab Rat - GWJ 2020/Scenes/Rat/rat.gd
@@ -1,24 +1,21 @@
 extends KinematicBody
 
 var speed = 5
-
-var rotation_speed = 2.5
-
-
+var rotation_speed = 2.1
+var roty: float
 func _physics_process(delta):
 	var move = Vector3(0,-1,0)
 	if Input.is_action_pressed("ui_up"):
 		move.x += 1
 	elif Input.is_action_pressed("ui_down"):
-		move.x += -1
+		move.x -= 1
 	if Input.is_action_pressed("ui_right"):
-		self.rotation.y -= rotation_speed * delta
+		roty -= rotation_speed * delta
 	elif Input.is_action_pressed("ui_left"):
-		self.rotation.y += rotation_speed * delta
-
-	move = move.rotated(Vector3(0,1,0).normalized(),rotation.y)
+		roty += rotation_speed * delta
+	self.rotation.y = roty
+	move = move.rotated(Vector3(0,1,0).normalized(),roty)
 	move = move_and_slide(move*speed, Vector3(0,1,0))
-
 
 func _ready():
 	set_process_input(true)
