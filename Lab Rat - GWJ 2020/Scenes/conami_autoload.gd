b7ef53efa85ce26b245b5b0744742e7cd93cad4f
Christoph Winkler <support@neo-gamefactory.de>
2020-04-17 21:45:11 +0200

    Fix Easter Egg Bug / Kollision mit unsichtbarer 'Wand'

--- Lab Rat - GWJ 2020/Scenes/conami_autoload.gd
+++ Lab Rat - GWJ 2020/Scenes/conami_autoload.gd
@@ -3,25 +3,11 @@ extends Node
 
 var konami = ["up", "up", "down", "down", "left", "right", "left", "right", "B", "A"] 
 var lastet_inputs =[]
-var easterEggStarted = false
-var timer = Timer.new()
-var easterEggScene = "res://Scenes/Easter Egg/easter egg.tscn"
 
-
-func _ready():
-	timer.name = "timer"
-	add_child(timer)
-	timer.connect("timeout", self, "_timer_finish")
-
-func _timer_finish():
-	easterEggStarted = true
-	timer.stop()
-	get_tree().change_scene(easterEggScene)
 	
 
 func _input(event):
-	timer = get_node("timer")
-	var action = true
+
 	
 	if event.is_action_pressed("ui_up"):
 		lastet_inputs.append("up")
@@ -36,12 +22,7 @@ func _input(event):
 			lastet_inputs.append("A")
 		elif event.scancode == KEY_B:
 			lastet_inputs.append("B")
-	else:
-		action = false
-		if timer.time_left == 0 and easterEggStarted == false:
-			timer.start(4)
-	if action:
-		timer.stop()
+
 
 	if len(lastet_inputs) > len(konami):
 		lastet_inputs.pop_front()
