f64827fbbc85646457921c1ff878984461a5e6ed
HugeGameArtGD <hugegameartgd@gmail.com>
2020-04-19 20:02:04 +0200

    bessere Licht und Labyrinth im Menü

--- Lab Rat - GWJ 2020/Scenes/Menus/HauptMenu/HauptMenu.gd
+++ Lab Rat - GWJ 2020/Scenes/Menus/HauptMenu/HauptMenu.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+extends Spatial
 
 
 
@@ -7,14 +7,15 @@ var startGame = "res://Scenes/Level/level-jonathan/level-jonathan.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
+func _physics_process(delta):
+	get_node("labyrinth").rotate_y(0.1*delta)
 
 func _on_NewGame_button_down():
 	get_tree().change_scene(startGame)
